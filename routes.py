c40372ec215c5ef07b31fe9f79f6ca0a0f93ca09
Camilo-Chico <aguja.2003@gmail.com>
2021-03-29 08:59:39 -0500

    Se hizo las rutas de extraccion del token

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
-from controllers import LoginUserControllers,RegisterUserControllers,Productos,app
+from controllers import LoginUserControllers,RegisterUserControllers,Productos,app,PedidosUserControllers
 
 
 user = {
     "login_user": "/api/v01/user/login", "login_user_controllers": LoginUserControllers.as_view("login_api"),
     "Register_user": "/api/v01/user/register", "login_register_controllers": RegisterUserControllers.as_view("register_api"),
-    "Productos_clients":"/api/v01/user/product","productos":Productos.as_view("productos_api")
+    "Productos_clients":"/api/v01/user/product","productos":Productos.as_view("productos_api"),
+    "Productos_clients_pedidos":"/api/v01/user/getInfo","productosPedidos":PedidosUserControllers.as_view("pedidos"),
 }
 
